a09139aed625f00c5f9b7cc3339742f6488effe5
Oleg Solomka <olsolomk@microsoft.com>
2020-08-18 20:07:30 -0700

    Update codespace-post-create.sh

--- .devcontainer/codespace-post-create.sh
+++ .devcontainer/codespace-post-create.sh
@@ -1,152 +1,144 @@
-export NODE_VERSION=12.18.1
-# install NodeJS
-curl -sL https://deb.nodesource.com/setup_12.x -o ~/nodesource_setup.sh && bash ~/nodesource_setup.sh && apt install nodejs -y
-# install yarn
-npm i -g yarn
+exec &>> creation.log
 
-# install Azure Credentials Provider for NuGet
-wget -c https://github.com/microsoft/artifacts-credprovider/releases/download/v0.1.22/Microsoft.NuGet.CredentialProvider.tar.gz -O - | tar -xz -C ~/.nuget
+echo "|================ RUN ================="
+echo "|================ RUN ================="
+echo "|================ RUN ================="
+echo "|================ RUN ================="
+echo "|================ RUN ================="
+echo "|================ RUN ================="
 
-# add oh-my-bash
-wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O - | sh -C
-
-# connect AzDO `npm` feeds
-echo "
-; begin auth token
-//devdiv.pkgs.visualstudio.com/_packaging/VS/npm/registry/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/VS/npm/registry/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/_packaging/VS/npm/registry/:email=npm requires email to be set but doesn't use the value
-//devdiv.pkgs.visualstudio.com/_packaging/VS/npm/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/VS/npm/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/_packaging/VS/npm/:email=npm requires email to be set but doesn't use the value
-; end auth token
-
-; begin auth token
-//devdiv.pkgs.visualstudio.com/_packaging/NodeRepos/npm/registry/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/NodeRepos/npm/registry/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/_packaging/NodeRepos/npm/registry/:email=npm requires email to be set but doesn't use the value
-//devdiv.pkgs.visualstudio.com/_packaging/NodeRepos/npm/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/NodeRepos/npm/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/_packaging/NodeRepos/npm/:email=npm requires email to be set but doesn't use the value
-; end auth token
-
-; begin auth token
-//devdiv.pkgs.visualstudio.com/_packaging/Cascade/npm/registry/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/Cascade/npm/registry/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/_packaging/Cascade/npm/registry/:email=npm requires email to be set but doesn't use the value
-//devdiv.pkgs.visualstudio.com/_packaging/Cascade/npm/:username=devdiv
-//devdiv.pkgs.visualstudio.com/_packaging/Cascade/npm/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/_packaging/Cascade/npm/:email=npm requires email to be set but doesn't use the value
-; end auth token
-
-; begin auth token
-//devdiv.pkgs.visualstudio.com/DevDiv/_packaging/playwright/npm/registry/:username=devdiv
-//devdiv.pkgs.visualstudio.com/DevDiv/_packaging/playwright/npm/registry/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/DevDiv/_packaging/playwright/npm/registry/:email=npm requires email to be set but doesn't use the value
-//devdiv.pkgs.visualstudio.com/DevDiv/_packaging/playwright/npm/:username=devdiv
-//devdiv.pkgs.visualstudio.com/DevDiv/_packaging/playwright/npm/:_password=\${AZ_DO_PAT_BASE64}
-//devdiv.pkgs.visualstudio.com/DevDiv/_packaging/playwright/npm/:email=npm requires email to be set but doesn't use the value
-; end auth token
-" >> ~/.npmrc
-
-BASH_RC_FILE=~/.bashrc
-
-PRE_OMB_BASH_CONFIG=~/.bashrc.pre-oh-my-bash
-if [ -f $PRE_OMB_BASH_CONFIG ]; then
-  cat $PRE_OMB_BASH_CONFIG >> $BASH_RC_FILE
-  rm $PRE_OMB_BASH_CONFIG
+#input
+SERVER='server-linux-x64-web'
+COMMIT_ID='3d5b2fecf2e6788cb9877d7d868d964fbc3ecd53'
+UUID='3d5b2fecf2e6788cb9877d7d868d964fbc3ecd53'
+QUALITY='insider'
+EXTENSIONS=''
+echo "|================ vscode-install.sh Arguments ================="
+echo "|     SERVER: $SERVER"
+echo "|  COMMIT_ID: $COMMIT_ID"
+echo "|       UUID: $UUID"
+echo "|    QUALITY: $QUALITY"
+echo "| EXTENSIONS: $EXTENSIONS"
+echo "|=============================================================="
+#setup
+VSCH_HOME="$HOME"
+VCSH_TAR="vscode-$SERVER.tar.gz"
+VSCH_BIN_DIR="$VSCH_HOME/.vscode-remote/bin"
+VSCH_DIR="$VSCH_BIN_DIR/$COMMIT_ID"
+VSCH_LOGFILE="$VSCH_HOME/.vscode-remote/.$COMMIT_ID.log"
+if [ ! -d "$VSCH_DIR" ]; then
+	mkdir -p $VSCH_DIR
 fi
 
-# add .bashrc config
-echo "
-# workspace
-export CODESPACE_ROOT=$(pwd)
-export CSCLIENT=\"\$CODESPACE_ROOT/src/Portal/PortalWebsite/Src/Website\"
-export CSSERVER=\"\$CODESPACE_ROOT/src/services/containers/VsClk.Portal.WebSite\"
-alias cdroot='cd \$CODESPACE_ROOT'
-alias cdclient='cd \$CSCLIENT'
-alias cdserver='cd \$CSSERVER'
-alias do='dotnet'
-alias ya='yarn'
-# misc
-alias code='f() {
-    if code-insiders -v &> /dev/null; then
-      code-insiders \$@;
-    else
-      code \$@;
-    fi
-};f'
-alias ls='ls --color=auto'
-alias ww='watch -n 1 \"date && echo -e \ &&\"'
-alias refresh='exec bash'
-alias bashconfig=\"code $BASH_RC_FILE\"
-alias ports='lsof -n -i -P | grep TCP'
-# git
-alias push='git push -u azdo HEAD'
-alias pull='git pull azdo'
-alias sync='pull && push'
-alias fetch='git fetch azdo'
-alias pullmaster='git pull azdo master'
-alias branch='f() {
-    BRANCH_NAME=\"dev/\$AZ_DO_USERNAME/\$1\";
-    git pull azdo master:main --no-tags;
-    git branch \$BRANCH_NAME main --color;
-    git checkout \$BRANCH_NAME;
-    git push -u azdo \$BRANCH_NAME;
-};f'
+## Copyright (C) 2009 Przemyslaw Pawelczyk <przemoc@gmail.com>
+## This script is licensed under the terms of the MIT license.
+## https://opensource.org/licenses/MIT
 
-## Color variables
+get_lockfile() {
+	echo "$VSCH_DIR/vscode-remote-lock.$1"
+}
 
-# Dimmed text
-export PALETTE_DIM='\e[2m'
+# PRIVATE
+_lock()             { flock -$1 $2; }
+_no_more_locking()  { _lock u $2; _lock xn $2 && rm -f $(get_lockfile $1); }
+_prepare_locking()  { eval "exec $2>\\"$(get_lockfile $1)\\""; trap "_no_more_locking $1 $2" EXIT; }
+# PUBLIC - all take lock FD
+exlock_now()        { _lock xn $1; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x $1; }   # obtain an exclusive lock
+shlock()            { _lock s $1; }   # obtain a shared lock
+unlock()            { _lock u $1; }   # drop a lock
 
-# Bold Text
-export PALETTE_BOLD='\e[1m'
+LOCKFD=99
+CLEANUP_LOCKFD=98
+_prepare_locking $COMMIT_ID $LOCKFD
 
-# Underlined Text
-export PALETTE_UNDERLINED='\e[4m'
-
-# Blinking
-export PALETTE_BLINK='\e[5m'
-
-# Reverse
-export PALETTE_REVERSE='\e[7m'
-
-# Foreground Color
-export PALETTE_BLACK='\e[30m'
-export PALETTE_WHITE='\e[97m'
-export PALETTE_RED='\e[31m'
-export PALETTE_GREEN='\e[32m'
-export PALETTE_BROWN='\e[33m'
-export PALETTE_BLUE='\e[34m'
-export PALETTE_PURPLE='\e[35m'
-export PALETTE_CYAN='\e[36m'
-export PALETTE_LIGHTGRAY='\e[37m'
-export PALETTE_LIGHT_YELLOW='\e[93m'
-
-# Background Color
-export PALETTE_BLACK_U='\e[40m'
-export PALETTE_RED_U='\e[41m'
-export PALETTE_GREEN_U='\e[42m'
-export PALETTE_BROWN_U='\e[43m'
-export PALETTE_BLUE_U='\e[44m'
-export PALETTE_PURPLE_U='\e[45m'
-export PALETTE_CYAN_U='\e[46m'
-export PALETTE_LIGHTGRAY_U='\e[47m'
-
-# Normal Text
-export PALETTE_RESET='\e[0m'
-
-# change dir to the \`Website\` folder if present (codespace is initialized),
-# otherwise show the hint
-if [ -d \$CSCLIENT ]; then
-  cd \$CSCLIENT
-elif [ \$(basename \"\$0\") != 'bootstrap' ]
+if (( $? > 0 ))
 then
-  clear
-  echo -e \"\$PALETTE_DIM\n💡  Run\$PALETTE_BLUE script/bootstrap\$PALETTE_RESET\$PALETTE_DIM when ready.\n\$PALETTE_RESET\"
+	echo "Installation already in progress..."
+	echo "$UUID##24##$UUID"
+	exit 0
 fi
 
-" >> $BASH_RC_FILE
+# Keep the newest 5 servers
+TO_DELETE=$(ls -1 --sort=time $VSCH_BIN_DIR | tail -n +6)
+for COMMIT_TO_DELETE in $TO_DELETE; do
+	echo "Found old VS Code install $COMMIT_TO_DELETE, attempting to clean up"
 
-exec bash
+	_prepare_locking $COMMIT_TO_DELETE $CLEANUP_LOCKFD
+	exlock_now $CLEANUP_LOCKFD
+	if (( $? == 0 )); then
+		RUNNING="`ps ax | grep $COMMIT_TO_DELETE | grep -v grep | wc -l | tr -d '[:space:]'`"
+		if [ "$RUNNING" = "0" ]; then
+			echo "Deleting old install from $VSCH_BIN_DIR/$COMMIT_TO_DELETE"
+			rm -rf $VSCH_BIN_DIR/$COMMIT_TO_DELETE
+		else
+			echo "Install still has running processes, not deleting: $COMMIT_TO_DELETE"
+ 		fi
+	else
+		echo "Failed to acquire lock for install, not deleting: $COMMIT_TO_DELETE"
+	fi
+done
+
+# install if needed
+if [ ! -f "$VSCH_DIR/server.sh" ]
+then
+	echo "Installing..."
+	STASHED_WORKING_DIR="`pwd`"
+	cd $VSCH_DIR
+	which wget &> /dev/null
+	if [ $? == 0 ]
+	then
+		echo "Downloading with wget"
+		WGET_ERRORS=$(2>&1 wget -nv -O $VCSH_TAR https://update.code.visualstudio.com/commit:$COMMIT_ID/$SERVER/$QUALITY)
+		if [ $? -ne 0 ]; then
+			echo $WGET_ERRORS
+			echo "$UUID##25##$UUID"
+			exit 0
+		fi
+	else
+		which curl &> /dev/null
+		if [ $? == 0 ]
+		then
+			echo "Downloading with curl"
+			CURL_OUTPUT=$(2>&1 curl -L -s -S https://update.code.visualstudio.com/commit:$COMMIT_ID/$SERVER/$QUALITY --output $VCSH_TAR -w "%{http_code}")
+			if [[ ($? -ne 0) || ($CURL_OUTPUT != 2??) ]]; then
+				echo $CURL_OUTPUT
+				echo "$UUID##25##$UUID"
+				exit 0
+			fi
+		else
+			echo "Neither wget nor curl is installed"
+			echo "$UUID##26##$UUID"
+			exit 0
+		fi
+	fi
+	tar -xf $VCSH_TAR --strip-components 1
+	if [ $? -gt 0 ]
+       	then
+		echo "WARNING: tar exited with non-0 exit code"
+	fi
+
+	# cheap sanity check
+	if [ ! -f $VSCH_DIR/node ]
+	then
+		echo "WARNING: $VSCH_DIR/node doesn't exist. Download/untar may have failed."
+	fi
+    if [ ! -f "$VSCH_DIR/server.sh" ]
+	then
+		echo "WARNING: "$VSCH_DIR/server.sh" doesn't exist. Download/untar may have failed."
+	fi
+	rm $VCSH_TAR
+	cd $STASHED_WORKING_DIR
+else
+	echo "Found existing installation..."
+fi
+
+# if [ ! -z "$EXTENSIONS" ]
+# 	then
+# 		echo "Installing extensions..."
+# 		$VSCH_DIR/server.sh $EXTENSIONS
+# fi
+
+echo "$UUID==$PORT==$UUID"
+
+unlock $LOCKFD
